21729b6ffec7a0c930119800aaeb9679011179f1
Michael Andersen <mian@kfst.dk>
2020-10-21 14:37:12 +0200

    Clean train_word2vec_dawiki.py
      - Removes commented code

--- train_word2vec_dawiki.py
+++ train_word2vec_dawiki.py
@@ -1,23 +1,18 @@
 from gensim.models.doc2vec import TaggedDocument
 from gensim.corpora.wikicorpus import WikiCorpus
 from gensim.models import Doc2Vec
-# class gensim.corpora.wikicorpus.WikiCorpus(fname
-# <LANG>wiki-<YYYYMMDD>-pages-articles.xml.bz2
-
 
 DATA_PATH = '../wiki/da/'
 DAWIKI = 'dawiki-20201020-pages-articles.xml.bz2'
 
 import os
 import sys
-# import bz2
 import logging
 import multiprocessing
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 MODEL_PATH  = os.path.join(SCRIPT_PATH, 'model/')
 
-DICTIONARY_FILEPATH = os.path.join(DATA_PATH, 'wiki-danish_wordids.txt.bz2')
 WIKI_DUMP_FILEPATH = os.path.join(DATA_PATH, DAWIKI)
 
 
@@ -29,7 +24,6 @@ if __name__ == '__main__':
         print('Please see README.md for instructions!')
         sys.exit()
 
-
     # Get number of available cpus
     cores = multiprocessing.cpu_count()
 
@@ -39,26 +33,6 @@ if __name__ == '__main__':
     # Initialize logging
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-    # if not os.path.isfile(DICTIONARY_FILEPATH):
-    #     logging.info('Dictionary has not been created yet..')
-    #     logging.info('Creating dictionary (takes about 9h)..')
-
-    #     # Construct corpus
-    #     wiki = WikiCorpus(WIKI_DUMP_FILEPATH)
-
-    #     # Remove words occuring less than 20 times, and words occuring in more
-    #     # than 10% of the documents. (keep_n is the vocabulary size)
-    #     wiki.dictionary.filter_extremes(no_below=20, no_above=0.1, keep_n=100000)
-
-    #     # Save dictionary to file
-    #     wiki.dictionary.save_as_text(DICTIONARY_FILEPATH)
-    #     del wiki
-
-    # # Load dictionary from file
-    # dictionary = gensim.corpora.Dictionary.load_from_text(DICTIONARY_FILEPATH)
-
-    # Construct corpus using dictionary
-    # wiki = gensim.corpora.WikiCorpus(WIKI_DUMP_FILEPATH, dictionary=dictionary)
     wiki = WikiCorpus(WIKI_DUMP_FILEPATH)
 
     class TaggedWikiDocumentIterator:
@@ -70,22 +44,11 @@ if __name__ == '__main__':
             for content, (_, title) in self.wiki.get_texts():
                 yield TaggedDocument(content, [title])
 
-    # class SentencesIterator:
-    #     def __init__(self, wiki):
-    #         self.wiki = wiki
 
-    #     def __iter__(self):
-    #         for sentence in self.wiki.get_texts():
-    #             yield list(sentence)
-
-    # Initialize simple sentence iterator required for the Word2Vec model
-    # sentences = SentencesIterator(wiki)
+    # Initialize TaggedWikiDocument iterator for the Doc2Vec model
     tagged_documents = TaggedWikiDocumentIterator(wiki=wiki)
 
-    # logging.info('Training word2vec model..')
-    # model = gensim.models.Word2Vec(sentences=, size=300, min_count=1, window=5, workers=cores)
-    # model = gensim.models.Word2Vec(sentences=sentences, size=300, min_count=1, window=5, workers=cores)
-
+    # Example model from https://github.com/RaRe-Technologies/gensim/blob/develop/docs/notebooks/doc2vec-wikipedia.ipynb
     # models = [
     #     # PV-DBOW 
     #     Doc2Vec(dm=0, dbow_words=1, size=200, window=8, min_count=19, iter=10, workers=cores),
@@ -105,4 +68,3 @@ if __name__ == '__main__':
     logging.info('Saving model..')
     model.save(os.path.join(MODEL_PATH, 'dawiki_dbow_vs300_mc17_epochs10_window_8.model'))
     logging.info('Model saved')
-    # logging.info('Done training word2vec model!')
